190554e8972b0505c0b8cc09548437f387e8e97b
Forrest Walker <forrestpatwalker@hotmail.com>
2021-05-24 15:19:22 -0600

    Update settings.py

--- fpwlibrary/fpwlibrary/settings.py
+++ fpwlibrary/fpwlibrary/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^@oxnjxwrqi9&up$v&4w5f7lzq7^4v8)dju=(8@81g1k**dfq&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
